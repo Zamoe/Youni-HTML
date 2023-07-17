3d3cd57ca30c385b7f2c4653293d22593287d004
Zamoe <66330015+Zamoe@users.noreply.github.com>
2023-07-18 00:44:53 +0200

    Commit from TeleportHQ

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import './style.css'
+import Home from './views/home'
+import About from './views/about'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route component={Home} exact path="/" />
+        <Route component={About} exact path="/about" />
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
